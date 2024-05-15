12fded92b0041a4a9e93f3d5535d8a8686192349
yonatanlla <yonatanllanto174@gmail.com>
2024-05-14 21:59:59 -0500

    terminado el ejerccio de for

--- principiantes/While/desafio/main.go
+++ principiantes/While/desafio/main.go
@@ -1,19 +1,1 @@
 package main
-
-import "fmt"
-
-func main() {
-	var num int
-	fmt.Print("Ingrese un número: ")
-	fmt.Scan(&num)
-
-	init := 0
-	res := 0
-
-	for init <= 10 {
-		res = init * num
-		fmt.Println(num, "*", init, "=", res)
-		init++
-	}
-
-}
