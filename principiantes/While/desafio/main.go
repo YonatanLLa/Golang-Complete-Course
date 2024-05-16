187ddd4a472edabf57b35b1b29e02fd1e965c657
yonatanlla <yonatanllanto174@gmail.com>
2024-05-16 18:59:44 -0500

    ejercicios terminado

--- principiantes/While/desafio/main.go
+++ principiantes/While/desafio/main.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func main() {
+	var number int
+	fmt.Print("Ingrese el numero: ")
+	fmt.Scan(&number)
+	count := 0
+	originalNumber := number
+	sum := 0
+	for number != 0 {
+		dig := number % 10
+		fmt.Println(dig, "mod")
+		sum += dig
+		number /= 10
+		fmt.Println(number, "div")
+		count++
+	}
+	fmt.Printf("La suma de los dígitos del número %d es: %d\n", originalNumber, sum)
+}
